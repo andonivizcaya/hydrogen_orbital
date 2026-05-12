aa41be44563776f9566e92c66c59bc5fee58395a
Vizcaya <andoni.vizcaya@sovos.com>
2026-05-12 19:30:11 -0400

    fix: I forgor some memstuff

--- src/hydrogen.h
+++ src/hydrogen.h
@@ -84,6 +84,61 @@ static inline void *memcpy(void *restrict dest, const void *restrict src, size_t
     return dest;
 }
 
+void *memset(void *dest, int c, size_t n)
+{
+	unsigned char *s = dest;
+	size_t k;
+
+	/* Fill head and tail with minimal branching. Each
+	 * conditional ensures that all the subsequently used
+	 * offsets are well-defined and in the dest region. */
+
+	if (!n) return dest;
+	s[0] = c;
+	s[n-1] = c;
+	if (n <= 2) return dest;
+	s[1] = c;
+	s[2] = c;
+	s[n-2] = c;
+	s[n-3] = c;
+	if (n <= 6) return dest;
+	s[3] = c;
+	s[n-4] = c;
+	if (n <= 8) return dest;
+
+	/* Advance pointer to align it at a 4-byte boundary,
+	 * and truncate n to a multiple of 4. The previous code
+	 * already took care of any head/tail that get cut off
+	 * by the alignment. */
+
+	k = -(uintptr_t)s & 3;
+	s += k;
+	n -= k;
+	n &= -4;
+
+	/* Pure C fallback with no aliasing violations. */
+	for (; n; n--, s++) *s = c;
+
+	return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	char *d = dest;
+	const char *s = src;
+
+	if (d==s) return d;
+	if ((uintptr_t)s-(uintptr_t)d-n <= -2*n) return memcpy(d, s, n);
+
+	if (d<s) {
+		for (; n; n--) *d++ = *s++;
+	} else {
+		while (n) n--, d[n] = s[n];
+	}
+
+	return dest;
+}
+
 void *wasm_realloc(void *old, size_t new_nbytes)
 {
     if (new_nbytes == 0) {
@@ -127,10 +182,10 @@ static inline size_t hydrogen_cstrlen(const char *s)
         }                     \
     } while (0)
 
-#define FREE      wasm_free
-#define MALLOC    wasm_malloc
+#define FREE wasm_free
+#define MALLOC wasm_malloc
 #define MEMCPY(d, s, n) memcpy((d), (s), (n))
-#define REALLOC   wasm_realloc
+#define REALLOC wasm_realloc
 #define STRLEN(s) hydrogen_cstrlen((s))
 
 __attribute__((noreturn)) static inline void exit(int status)
